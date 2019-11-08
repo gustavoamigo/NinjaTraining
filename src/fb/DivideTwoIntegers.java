44879296984eda7b694a13d46e912335c174259d
Gustavo Amigo <gustavo.amigo@gmail.com>
2019-11-07 21:26:39 -0300

    fb

--- src/fb/DivideTwoIntegers.java
+++ src/fb/DivideTwoIntegers.java
@@ -2,63 +2,33 @@ package fb;
 
 // https://leetcode.com/explore/interview/card/facebook/54/sorting-and-searching-3/308/
 class DivideTwoIntegers {
+
   public int divide(int dividend, int divisor) {
-    if (dividend == 0) { return 0; }
+    int sign = (dividend < 0 && divisor > 0 || dividend > 0 && divisor < 0) ? -1 : 1;
 
-    final int result;
-    if (abs(divisor) == 1) {
-      result = abs(dividend);
+    long q = 0;
+    long dividendL = Math.abs((long) dividend);
+    long divisorL = Math.abs((long) divisor);
+    if (divisorL == 1) {
+      q = dividendL;
     } else {
-      result = search(abs(dividend), abs(divisor), 0, abs(dividend));
+      while (dividendL >= divisorL) {
+        dividendL -= divisorL;
+        q++;
+      }
     }
 
-    if (dividend < 0 && divisor > 0 || dividend > 0 && divisor < 0) {
-      return inv(result);
+
+    if (q > Integer.MAX_VALUE) {
+      return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
     } else {
-      return result;
-    }
-  }
-
-  private int abs(int i) {
-    if (i >= 0) {
-      return i;
-    } else {
-      return inv(i);
-    }
-  }
-
-  private int inv(int i) {
-    return 0 - i;
-  }
-
-  // dividend = 1 divisor = 2
-  private int search(int dividend, int divisor, int low, int high) { // low = 1 high = 2
-    if (dividend < divisor) { return 0; }
-    if (low == high) { return low; }
-    int diff = high - low; // diff = 1
-    int candidate = low + (diff >> 1); // candidate = 1 + 0 = 1
-    int sum = 0;
-    int i = 0;
-    while (sum < dividend) {  // i = 3 sum = 3
-      sum += candidate;
-      i++;
+      return (int) q * sign;
     }
 
-    if (sum > dividend) {
-      i--; // i = 1
-    }
-
-    if (i == divisor || i == (divisor + 1)) { // false
-      return candidate;
-    } else if (i > divisor) { // // i = 1 divisor = 2
-      return search(dividend, divisor, candidate, high);
-    } else {
-      return search(dividend, divisor, low, candidate);
-    }
   }
 
   public static void main(String[] args) {
     DivideTwoIntegers divideTwoIntegers = new DivideTwoIntegers();
-    System.out.println(divideTwoIntegers.divide(20, 5));
+    System.out.println(divideTwoIntegers.divide(20, 6));
   }
 }
